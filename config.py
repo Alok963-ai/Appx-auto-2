362076299df4dabd05139b61d50abdfe4090fdc5
Alok963-ai <alokjangde6@gmail.com>
2025-06-17 20:40:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ import os
 
 class Config(object):
     BOT_TOKEN = ""
-    DB_NAME = ""
-    API_ID = 
-    API_HASH = ""
-    ADMIN_ID = 
+    DB_NAME = "alokjangde"
+    API_ID = 26572696
+    API_HASH = "67a8947a3e1b15f9ef9684286baa34cb"
+    ADMIN_ID = 7903596276
     DB_URL = ""
-    LOG_CHANNEL =  # Your Log Channel ID (Bot ko ADMIN BNAYE)
+    LOG_CHANNEL = 2437405343 # Your Log Channel ID (Bot ko ADMIN BNAYE)
     USERLINK = ""
     TUTORIAL_VIDEO = ""
